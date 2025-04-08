8132342ce295bd96a8b0f6d471518d43eb639cff
donghuison <131636080+donghuison@users.noreply.github.com>
2025-04-09 00:47:15 +0900

    first

--- meetings.js
+++ meetings.js
@@ -61,14 +61,13 @@ document.addEventListener('DOMContentLoaded', function() {
         ], video: 'https://khu-ac.zoom.us/rec/share/JP_JBwn2rXzXxqVRnyAJf9WEp3pSA3MAdPDzOXCJrGM6ea4lGdfLJ4VSZ4gCdoXd.XMYkZSGqfee5uwRg', ppt: 'TBD'},
         { date: '2025-03-14', presenter: 'Kyungsun Park', article: { title: 'Multispecies and Multifluid MHD Approaches for the Study of Ionospheric Escape at Mars', url: 'https://doi.org/10.1029/2017JA025117' }, video: 'https://khu-ac.zoom.us/rec/share/0P7irjW7v7Bpd3SlCi7ZhEQSCzbWGL_C56usbjBoevSU8QOvEVD-B9QSDiHLNDrm.q5Byp7l9EQvq7EMn', ppt: 'TBD'},
         { date: '2025-03-21', presenter: 'Youngjae Kim', article: { title: 'Applying the Weighted Horizontal Magnetic Gradient Method to a Simulated Flaring Active Region', url: 'https://iopscience.iop.org/article/10.3847/1538-4357/aab891/meta' }, video: 'https://khu-ac.zoom.us/rec/share/vU-Ur-HXGsMj6ooe09QG0gJv_XF1va7xKl70Lnf-zCE0PkCGzVL4LizdJUfFriZg.8e63ke1abtdJgqun', ppt: 'TBD'},
-        { date: '2025-03-28', presenter: 'Sibaek Yi', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD'},
-        { date: '2025-04-04', presenter: 'Donghui Son', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD'},
+        { date: '2025-03-28', presenter: 'Sibaek Yi', article: { title: 'A Data-constrained Model for Coronal Mass Ejections Using the Graduated Cylindrical Shell Method', url: 'https://iopscience.iop.org/article/10.3847/1538-4357/aad3b4/meta' }, video: 'https://khu-ac.zoom.us/rec/share/mBCZNlhl2EDsr8KNl-BUFx8lSQpy5IF1JE9tpuvaVn1JxnJaG6VafFfhvApxXX_Z.w5DZwUCOf9K7Y3sS', ppt: 'TBD'},
+        { date: '2025-04-04', presenter: 'Donghui Son', article: { title: 'Convective Magnetic Flux Emergence Simulations from the Deep Solar Interior to the Photosphere: Comprehensive Study of Flux Tube Twist', url: 'https://iopscience.iop.org/article/10.3847/1538-4357/ad7e1d' }, video: 'https://iopscience.iop.org/article/10.3847/1538-4357/ad7e1d', ppt: 'TBD'},
         { date: '2025-04-18', presenter: 'Jihye Kang', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD'},
         { date: '2025-05-09', presenter: 'Yeonwoo Jang', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD'},
         { date: '2025-05-16', presenter: 'Mingyu Jeon', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD'},
         { date: '2025-05-23', presenter: 'Junmo An', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD'},
     ];
-
     
 
     // 미팅 그룹의 시작과 끝 인덱스를 명확하게 정의
