0af115de39111efa73a08600e35178e3e640f737
donghuison <dh5243@khu.ac.kr>
2026-03-08 20:41:27 +0900

    Add 2026 first meeting data and update index to include new entries

---
+++ meetings-data/2026-1st.js
@@ -0,0 +1,15 @@
+export default {
+    id: '2026-1st',
+    meetings: [
+        { date: '2026-03-13', presenter: 'Mingyu Jeon', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-03-20', presenter: 'Donghui Son', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-03-27', presenter: 'Seungwoo Ahn', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-04-03', presenter: 'Youngjae Kim', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-04-17', presenter: 'Junmo An', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-05-08', presenter: 'Sibaek Yi', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-05-15', presenter: 'Daeil Kim', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-05-22', presenter: 'Jihye Kang', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-05-29', presenter: 'Yeonwoo Jang', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-06-05', presenter: 'Sangjin Park', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+    ]
+};
--- meetings-data/index.js
+++ meetings-data/index.js
@@ -4,6 +4,7 @@ import meetings2024Second from './2024-2nd.js';
 import meetings2024Third from './2024-3rd.js';
 import meetings2025First from './2025-1st.js';
 import meetings2025Second from './2025-2nd.js';
+import meetings2026First from './2026-1st.js';
 
 export const meetingGroups = [
     meetings2023First,
@@ -11,7 +12,8 @@ export const meetingGroups = [
     meetings2024Second,
     meetings2024Third,
     meetings2025First,
-    meetings2025Second
+    meetings2025Second,
+    meetings2026First
 ];
 
 export const allMeetings = meetingGroups.flatMap(group => group.meetings);
