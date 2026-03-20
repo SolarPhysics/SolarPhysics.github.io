1e549ea55014cbe5a5fb47c365d8e3971519186a
donghuison <dh5243@khu.ac.kr>
2026-03-20 20:19:17 +0900

    Update Donghui Son's article URL in 2026 first meeting data for accuracy

--- meetings-data/2026-1st.js
+++ meetings-data/2026-1st.js
@@ -2,7 +2,7 @@ export default {
     id: '2026-1st',
     meetings: [
         { date: '2026-03-13', presenter: 'Mingyu Jeon', article: { title: 'Evaluating Solar Wind Forecast Using Magnetic Maps That Include Helioseismic Far-Side Information', url: 'https://link.springer.com/article/10.1007/s11207-025-02563-4'}, video: 'https://khu-ac.zoom.us/rec/share/hTSCnlsfVfXg1UdWW6XLc603ELyLguvLXyvQ6bx2j6jlEVurhZUJ8I_lKr1QQm82.Lh3nzzclAuwZvoqh', ppt: 'TBD' },
-        { date: '2025-03-20', presenter: 'Donghui Son', article: { title: 'A model for heating the super-hot corona in solar active regions', url: 'A model for heating the super-hot corona in solar active regions' }, video: 'https://khu-ac.zoom.us/rec/share/TtJ0qasyeE1O9pp9Ux1diARHNSlwWy5Tdliyasphm8R7kEjFQP3u88JphmrdiW_m.Wz9RGmMaQeU5mH91', ppt: 'TBD' },
+        { date: '2025-03-20', presenter: 'Donghui Son', article: { title: 'A model for heating the super-hot corona in solar active regions', url: 'https://doi.org/10.1038/s41550-024-02244-5' }, video: 'https://khu-ac.zoom.us/rec/share/TtJ0qasyeE1O9pp9Ux1diARHNSlwWy5Tdliyasphm8R7kEjFQP3u88JphmrdiW_m.Wz9RGmMaQeU5mH91', ppt: 'TBD' },
         { date: '2025-03-27', presenter: 'Seungwoo Ahn', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
         { date: '2025-04-03', presenter: 'Youngjae Kim', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
         { date: '2025-04-17', presenter: 'Junmo An', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
