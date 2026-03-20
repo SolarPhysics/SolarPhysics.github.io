7cdf35d0a9a98c24d007ae2528f4ded18edc49cf
donghuison <dh5243@khu.ac.kr>
2026-03-20 20:17:38 +0900

    Update 2026 first meeting data with article titles and video links for Mingyu Jeon and Donghui Son

--- meetings-data/2026-1st.js
+++ meetings-data/2026-1st.js
@@ -1,15 +1,15 @@
 export default {
     id: '2026-1st',
     meetings: [
-        { date: '2026-03-13', presenter: 'Mingyu Jeon', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2025-03-20', presenter: 'Donghui Son', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2025-03-27', presenter: 'Seungwoo Ahn', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2025-04-03', presenter: 'Youngjae Kim', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2025-04-17', presenter: 'Junmo An', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2025-05-08', presenter: 'Sibaek Yi', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2025-05-15', presenter: 'Daeil Kim', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2026-05-22', presenter: 'Jihye Kang', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2026-05-29', presenter: 'Yeonwoo Jang', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
-        { date: '2026-06-05', presenter: 'Sangjin Park', article: { title: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-03-13', presenter: 'Mingyu Jeon', article: { title: 'Evaluating Solar Wind Forecast Using Magnetic Maps That Include Helioseismic Far-Side Information', url: 'https://link.springer.com/article/10.1007/s11207-025-02563-4'}, video: 'https://khu-ac.zoom.us/rec/share/hTSCnlsfVfXg1UdWW6XLc603ELyLguvLXyvQ6bx2j6jlEVurhZUJ8I_lKr1QQm82.Lh3nzzclAuwZvoqh', ppt: 'TBD' },
+        { date: '2025-03-20', presenter: 'Donghui Son', article: { title: 'A model for heating the super-hot corona in solar active regions', url: 'A model for heating the super-hot corona in solar active regions' }, video: 'https://khu-ac.zoom.us/rec/share/TtJ0qasyeE1O9pp9Ux1diARHNSlwWy5Tdliyasphm8R7kEjFQP3u88JphmrdiW_m.Wz9RGmMaQeU5mH91', ppt: 'TBD' },
+        { date: '2025-03-27', presenter: 'Seungwoo Ahn', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-04-03', presenter: 'Youngjae Kim', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-04-17', presenter: 'Junmo An', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-05-08', presenter: 'Sibaek Yi', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2025-05-15', presenter: 'Daeil Kim', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-05-22', presenter: 'Jihye Kang', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-05-29', presenter: 'Yeonwoo Jang', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-06-05', presenter: 'Sangjin Park', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
     ]
 };
