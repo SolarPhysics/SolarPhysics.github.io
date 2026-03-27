e26e8f4a9ec0609d27fc6cfd3600f4bf5d427e8a
donghuison <dh5243@khu.ac.kr>
2026-03-28 04:07:15 +0900

    Update meeting details for March 27, 2026, with new article information and video link

--- meetings-data/2026-1st.js
+++ meetings-data/2026-1st.js
@@ -3,7 +3,7 @@ export default {
     meetings: [
         { date: '2026-03-13', presenter: 'Mingyu Jeon', article: { title: 'Evaluating Solar Wind Forecast Using Magnetic Maps That Include Helioseismic Far-Side Information', url: 'https://link.springer.com/article/10.1007/s11207-025-02563-4'}, video: 'https://khu-ac.zoom.us/rec/share/hTSCnlsfVfXg1UdWW6XLc603ELyLguvLXyvQ6bx2j6jlEVurhZUJ8I_lKr1QQm82.Lh3nzzclAuwZvoqh', ppt: 'TBD' },
         { date: '2026-03-20', presenter: 'Donghui Son', article: { title: 'A model for heating the super-hot corona in solar active regions', url: 'https://doi.org/10.1038/s41550-024-02244-5' }, video: 'https://khu-ac.zoom.us/rec/share/TtJ0qasyeE1O9pp9Ux1diARHNSlwWy5Tdliyasphm8R7kEjFQP3u88JphmrdiW_m.Wz9RGmMaQeU5mH91', ppt: 'TBD' },
-        { date: '2026-03-27', presenter: 'Seungwoo Ahn', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
+        { date: '2026-03-27', presenter: 'Seungwoo Ahn', article: { title: 'A prolific solar flare factory', url: 'https://doi.org/10.1051/0004-6361/202554323' }, video: 'https://khu-ac.zoom.us/rec/share/HS7ezjy751Q8hKsPe464EFyDkB2qwdT7dCfFd8B7b51une7US5SKrD9sdfthr3PU.ZVL4GVI0t-dHWPTp', ppt: 'TBD' },
         { date: '2026-04-03', presenter: 'Youngjae Kim', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
         { date: '2026-04-17', presenter: 'Junmo An', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
         { date: '2026-05-08', presenter: 'Sibaek Yi', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
@@ -13,3 +13,5 @@ export default {
         { date: '2026-06-05', presenter: 'Sangjin Park', article: { title: 'TBD', url: 'TBD' }, video: 'TBD', ppt: 'TBD' },
     ]
 };
+
+
